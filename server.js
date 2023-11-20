b533543b29ce77288d894168ad82fd370e4917a9
GURU SENSEI <sahoogurucharan00@gmail.com>
2023-11-20 11:04:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import fs from 'fs';
 import {generate} from 'random-words';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 const filePath = 'pastes.json';
