85dee3591bdf3be03e879dd3f711a8a1b7bc4f39
GURU SENSEI <sahoogurucharan00@gmail.com>
2023-11-25 21:45:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import fs from 'fs';
-import { generate } from 'random-words';
 import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios'; 
 
 const app = express();
 const port = 3000;
-const filePath = 'pastes.json';
+const dbUrl = 'https://gurupaste.gurucharan-saho.repl.co'; 
 
 app.use(cors());
 app.use(bodyParser.json());
-
-// Asynchronous file operations
-async function loadPastes() {
-  try {
-    const data = await fs.promises.readFile(filePath, 'utf8');
-    return JSON.parse(data);
-  } catch (err) {
-    console.error('Error reading pastes file:', err.message);
-    return [];
-  }
-}
-
-async function savePastes(pastes) {
-  try {
-    await fs.promises.writeFile(filePath, JSON.stringify(pastes, null, 2), 'utf8');
-  } catch (err) {
-    console.error('Error writing pastes file:', err.message);
-  }
-}
-
-// Middleware for logging
-app.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  next();
-});
 
 // Validation middleware
 function validateInput(req, res, next) {
@@ -58,20 +32,32 @@ app.get('/pastes', async (req, res) => {
 
   if (action === 'create') {
     const content = req.query.content;
-    const pastes = await loadPastes();
     const paste = { id: generatePasteId(), content };
-    pastes.push(paste);
-    await savePastes(pastes);
-    res.status(201).json({ id: paste.id });
+    
+    try {
+     
+      await axios.post(`${dbUrl}/pastes`, paste);
+      res.status(201).json({ id: paste.id });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   } else if (action === 'getpaste') {
     const id = req.query.id;
-    const pastes = await loadPastes();
-    const paste = pastes.find((p) => p.id === id);
 
-    if (paste) {
-      res.json(paste);
-    } else {
-      res.status(404).json({ error: 'Paste not found' });
+    try {
+      
+      const response = await axios.get(`${dbUrl}/pastes/${id}`);
+      const paste = response.data;
+
+      if (paste) {
+        res.json(paste);
+      } else {
+        res.status(404).json({ error: 'Paste not found' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
   } else {
     res.status(400).json({ error: 'Invalid action' });
